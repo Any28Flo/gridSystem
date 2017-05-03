a79212e368e8b82433b41a0967a941c6dfed25a0
“Erika <foglzerika@gmail.com>
2017-05-03 16:19:33 -0500

    Arreglando el bug de funcion cerrar js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
  * Archivo principal de funcionalidad de JS
  */
  var muestraImagen= document.getElementsByClassName('workImage');
-console.log(typeof(eventos));
 
 [].forEach.call(muestraImagen, function(elemento){
   elemento.addEventListener("click",ampliaImagen);
@@ -18,5 +17,5 @@ function ampliaImagen() {
    this.previousElementSibling.style.width="100%";
 }
 function closeNav() {
-  console.log(this.siblings);
+  this.parentNode.style.width="0%";
 }
