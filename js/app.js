d91b5e9bb797af7489afef57d561091dc4f812d6
“Erika <foglzerika@gmail.com>
2017-05-03 15:58:25 -0500

    Agregando javascript funcionalidad mostrar imagen en grande

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ var muestraImagen= document.getElementsByClassName('workImage');
+console.log(typeof(eventos));
+
+[].forEach.call(muestraImagen, function(elemento){
+  elemento.addEventListener("click",ampliaImagen);
+})
+
+ var ocultaImagen=document.getElementsByClassName("closebtn");
+ [].forEach.call(ocultaImagen,function(elemento){
+   elemento.addEventListener("click",closeNav);
+ })
+
+function ampliaImagen() {
+
+   this.previousElementSibling.style.width="100%";
+}
+function closeNav() {
+  console.log(this.siblings);
+}
